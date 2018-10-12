cdc78eab98f0ffad8f1ef7296db981a550a2e85b
andeepak <andeepak@LIN80010704.corp.capgemini.com>
2018-10-12 12:19:48 +0530

    Data structure related changes implemented in resource file and committed.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-insert into friendmanagement(email, friend_list, subscription, updated_timestamp) values('soni.mukesh915@gmail.com', '', '', now());
-insert into friendmanagement(email, friend_list, subscription, updated_timestamp) values('ranga@gmail.com', '', '', now());insert into friendmanagement(email, friend_list, subscription, updated_timestamp) values('vishwanath@gmail.com', '', '', now());
-insert into friendmanagement(email, friend_list, subscription, updated_timestamp) values('som@gmail.com', '', '', now()); 
-insert into unsubscribe(Requestor_email, Target_email, Subscription_Status) values('soni.mukesh915@gmail.com', 'som@gmail.com', 'Blocked');
+insert into friendmanagement(email, friend_list, subscriber, subscribedBy, updated, updated_timestamp) values('john@example.com', '', '', '', '', now());
+insert into friendmanagement(email, friend_list, subscriber, subscribedBy, updated, updated_timestamp) values('andy@example.com', '', '', '', '', now());
+insert into friendmanagement(email, friend_list, subscriber, subscribedBy, updated, updated_timestamp) values('lily@example.com', '', '', '', '', now());
+insert into friendmanagement(email, friend_list, subscriber, subscribedBy, updated, updated_timestamp) values('lucy@example.com', '', '', '', '', now()); 
 
-
-insert into unsubscribe(Requestor_email, Target_email, Subscription_Status) values('vishwanath@gmail.com', 'ranga@gmail.com', 'Blocked');
+insert into unsubscribe(Requestor_email, Target_email, Subscription_Status) values('john@example.com', 'andy@example.com', 'Blocked');
+insert into unsubscribe(Requestor_email, Target_email, Subscription_Status) values('lily@example.com', 'lucy@example.com', 'Blocked');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,12 +1,18 @@
 CREATE TABLE friendManagement(
-Id varchar(5) not null AUTO_INCREMENT, 
+Id integer not null AUTO_INCREMENT, 
 email varchar(255) not null, 
 friend_list varchar(255), 
-subscription varchar(255), 
+subscriber varchar(255),
+subscribedBy varchar(255),
+updated varchar(255), 
 updated_timestamp timestamp, primary key(Id)); 
+
 
 CREATE TABLE UNSUBSCRIBE(
 Id integer not null AUTO_INCREMENT, 
 Requestor_email varchar(255), 
 Target_email varchar(255), 
 Subscription_Status varchar(50));
+
+
+
